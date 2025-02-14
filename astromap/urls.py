bc2dc42020d407b425d6e36e006e735849e5483d
Naakinn <fedortar8@gmail.com>
2025-02-14 20:38:38 +0300

    Add favicon

--- astromap/urls.py
+++ astromap/urls.py
@@ -1,7 +1,10 @@
+from django.views.generic import RedirectView
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('favicon.ico',RedirectView.as_view(url='/static/img/favicon.ico')),
+    
     path("", views.index_view, name="index_view"),
     path("ping/", views.ping, name="ping"),
     path("map/", views.map_view, name="map_view"),
